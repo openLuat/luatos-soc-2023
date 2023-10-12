f9bf2fedb1f210520d732b1d050e5d5070aa9bc6
Dozingfiretruck <1473454180@qq.com>
2023-10-12 12:54:45 +0800

    add:添加 rndis demo

---
+++ project/example_rndis/src/example_main.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 OpenLuat & AirM2M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "luat_network_adapter.h"
+#include "common_api.h"
+#include "luat_rtos.h"
+#include "luat_mem.h"
+#include "luat_debug.h"
+#include "luat_mobile.h"
+
+uint8_t soc_rndis_is_enable(void)
+{
+	return 1;
+}
+
+static void luatos_mobile_event_callback(LUAT_MOBILE_EVENT_E event, uint8_t index, uint8_t status)
+{
+	if (LUAT_MOBILE_EVENT_NETIF == event)
+	{
+		if (LUAT_MOBILE_NETIF_LINK_ON == status)
+		{
+			LUAT_DEBUG_PRINT("luatos_mobile_event_callback  link ...");
+			luat_socket_check_ready(index, NULL);
+		}
+        else if(LUAT_MOBILE_NETIF_LINK_OFF == status || LUAT_MOBILE_NETIF_LINK_OOS == status)
+        {
+            LUAT_DEBUG_PRINT("luatos_mobile_event_callback  error ...");
+        }
+	}
+}
+
+static void luat_example_init(void)
+{
+	luat_mobile_event_register_handler(luatos_mobile_event_callback);
+	net_lwip_init();
+	net_lwip_register_adapter(NW_ADAPTER_INDEX_LWIP_GPRS);
+	network_register_set_default(NW_ADAPTER_INDEX_LWIP_GPRS);
+}
+
+INIT_TASK_EXPORT(luat_example_init, "1");
+
