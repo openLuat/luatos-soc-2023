76543b2d2e9df58e5c8d2a493969950de7f2ec96
doudou <1491766334@qq.com>
2023-10-31 13:36:50 +0800

    update:先保证能编译通过

--- interface/src/luat_pwm_ec7xx.c
+++ interface/src/luat_pwm_ec7xx.c
@@ -25,6 +25,7 @@ typedef struct
 	uint32_t last_pulse_rate;
 	uint8_t update_period;
 	uint8_t reverse;
+	int stop_level;
 }luat_pwm_ctrl_t;
 
 static luat_pwm_ctrl_t g_s_pwm_table[PWM_CH_MAX] =
@@ -114,9 +115,9 @@ int luat_pwm_set_callback(int channel, CBFuncEx_t callback, void *param)
 }
 
 
-int luat_pwm_open(int channel, size_t freq,  size_t pulse, int pnum, uint8_t reverse, int stop_level) {
+int luat_pwm_open(int channel, size_t freq,  size_t pulse, int pnum) {
 	// uint8_t instance = channel % 10;
-	if ((channel >= PWM_CH_MAX) || (channel == 3) || (channel < 0) || (freq > 13000000) || (reverse > 3)) return -1;
+	if ((channel >= PWM_CH_MAX) || (channel == 3) || (channel < 0) || (freq > 13000000)) return -1;
 	CLOCK_setClockSrc(g_s_pwm_table[channel].id, g_s_pwm_table[channel].select);
 	CLOCK_setClockDiv(g_s_pwm_table[channel].id, 1);
 	XIC_DisableIRQ(g_s_pwm_table[channel].irq_line);
@@ -150,21 +151,21 @@ int luat_pwm_open(int channel, size_t freq,  size_t pulse, int pnum, uint8_t rev
 	}
 
 	EIGEN_TIMER(channel)->TIVR = 0;
-	uint8_t level = TIMER_PWM_STOP_HOLD;
-	switch (stop_level)
+	uint8_t stop_level = TIMER_PWM_STOP_HOLD;
+	switch (g_s_pwm_table[channel].stop_level)
 	{
 	case 0:
-		level = TIMER_PWM_STOP_LOW;
+		stop_level = TIMER_PWM_STOP_LOW;
 		break;
 	case 1:
-		level = TIMER_PWM_STOP_HIGH;
+		stop_level = TIMER_PWM_STOP_HIGH;
 		break;
 	default:
 		break;
 	}
 
 	/* Enable PWM out */
-	EIGEN_TIMER(channel)->TCTLR =  ((level << TIMER_TCTLR_PWM_STOP_VALUE_Pos) & TIMER_TCTLR_PWM_STOP_VALUE_Msk) | ((2 << TIMER_TCTLR_MCS_Pos) | TIMER_TCTLR_PWMOUT_Msk);
+	EIGEN_TIMER(channel)->TCTLR =  ((stop_level << TIMER_TCTLR_PWM_STOP_VALUE_Pos) & TIMER_TCTLR_PWM_STOP_VALUE_Msk) | ((2 << TIMER_TCTLR_MCS_Pos) | TIMER_TCTLR_PWMOUT_Msk);
 
     if(0 != pnum)
     {
@@ -211,8 +212,8 @@ int luat_pwm_open(int channel, size_t freq,  size_t pulse, int pnum, uint8_t rev
             break;
         }
     }
-	g_s_pwm_table[channel].reverse = reverse;
-	switch (reverse)
+
+	switch (g_s_pwm_table[channel].reverse)
     {
 	case 0 :
 		break;
@@ -325,8 +326,9 @@ int luat_pwm_setup(luat_pwm_conf_t* conf)
 			return 0;
 		}
 	}
-
-    return luat_pwm_open(conf->channel,conf->period,conf->pulse,conf->pnum, conf->reverse, conf->stop_level);
+	g_s_pwm_table[channel].reverse = conf->reverse;
+	g_s_pwm_table[channel].stop_level = conf->stop_level;
+    return luat_pwm_open(conf->channel,conf->period,conf->pulse,conf->pnum);
 }
 
 int luat_pwm_capture(int channel,int freq)
--- project/example_pwm/src/example_main.c
+++ project/example_pwm/src/example_main.c
@@ -10,10 +10,10 @@ luat_rtos_task_handle pwm_task_handle;
 
 static void task_test_pwm(void *param)
 {
-    luat_pwm_open(0, 8000, 500, 0, 0, -1);
-    luat_pwm_open(1, 8000, 500, 0, 0, -1);
-    luat_pwm_open(2, 8000, 500, 0, 0, -1);
-    luat_pwm_open(4, 8000, 500, 0, 0, -1);
+    luat_pwm_open(0, 8000, 500, 0);
+    luat_pwm_open(1, 8000, 500, 0);
+    luat_pwm_open(2, 8000, 500, 0);
+    luat_pwm_open(4, 8000, 500, 0);
     size_t pl = 0;
     while (1)
     {
