87dc7a05f89e7caf471bcc8846840b56f2a3de19
Wendal Chen <wendal1985@gmail.com>
2023-12-27 09:12:38 +0800

    add: luatos固件关联xxtea库

--- interface/src/luat_base_ec7xx.c
+++ interface/src/luat_base_ec7xx.c
@@ -265,6 +265,9 @@ static const luaL_Reg loadedlibs[] = {
 #ifdef LUAT_USE_WS2812
   {"ws2812", luaopen_ws2812},
 #endif
+#ifdef LUAT_USE_XXTEA
+  {"xxtea", luaopen_xxtea},
+#endif
   {NULL, NULL}
 };
 
