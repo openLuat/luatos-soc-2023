2622b211d2723a411465dab5708c1d61be4dba5c
Dozingfiretruck <1473454180@qq.com>
2024-01-25 16:13:16 +0800

    fix:编译失败

--- project/example_cjson/xmake.lua
+++ project/example_cjson/xmake.lua
@@ -8,5 +8,11 @@ target(TARGET_NAME)
     --加入代码和头文件
     add_includedirs("./inc",{public = true})
     add_files("./src/**.c")
+    
+    local chip_target = get_config("chip_target")
+    -- cjson
+	if chip_target ~= "ec718pv" then
+		add_files(LUATOS_ROOT.."/components/cjson/*.c")
+	end
 
 target_end()
