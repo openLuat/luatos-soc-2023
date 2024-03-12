4c936be63c671992a31970e65ce029603fec9076
alienwalker <alienwalker@sina.com>
2024-03-12 16:14:10 +0800

    update:i2s全双工demo打印一下数据

--- project/example_i2s/src/example_main.c
+++ project/example_i2s/src/example_main.c
@@ -11,16 +11,23 @@
 #include "luat_mem.h"
 
 #define TEST_I2S_ID I2S_ID0
+static luat_rtos_task_handle g_s_task_handle;
+
+enum
+{
+	I2S_RX_DONE = 1,
+	I2S_TX_DONE,
+};
 
 __USER_FUNC_IN_RAM__ int record_cb(uint8_t id ,luat_i2s_event_t event, uint8_t *rx_data, uint32_t rx_len, void *param)
 {
 	switch(event)
 	{
 	case LUAT_I2S_EVENT_RX_DONE:
-		DBG("%d", rx_len);
+		luat_rtos_event_send(g_s_task_handle, I2S_RX_DONE, (uint32_t)rx_data, rx_len, 0, 0);
 		break;
 	case LUAT_I2S_EVENT_TRANSFER_DONE:
-		DBG("!");
+		luat_rtos_event_send(g_s_task_handle, I2S_TX_DONE, 0, 0, 0, 0);
 		break;
 	default:
 		break;
@@ -28,34 +35,49 @@ __USER_FUNC_IN_RAM__ int record_cb(uint8_t id ,luat_i2s_event_t event, uint8_t *
 	return 0;
 }
 
-static uint8_t data[8000];
+static uint32_t test_data[2000];
 static void es8311_demo_task(void *arg)
 {
+	PV_Union upv;
 	luat_i2s_conf_t i2s_conf = {
 		.id = TEST_I2S_ID,
 		.mode = LUAT_I2S_MODE_MASTER,
 		.channel_format = LUAT_I2S_CHANNEL_RIGHT,
 		.standard = LUAT_I2S_MODE_LSB,
 		.channel_bits = LUAT_I2S_BITS_32,
-		.data_bits = LUAT_I2S_BITS_16,
+		.data_bits = LUAT_I2S_BITS_24,
 		.is_full_duplex = 1,
 		.sample_rate = 0,
 		.cb_rx_len = 8000,
 		.luat_i2s_event_callback = record_cb,
 	};
+	for(int i = 0; i < 2000; i++)
+	{
+		test_data[i] = 0x12345678;
+	}
 	luat_i2s_setup(&i2s_conf);
-	luat_i2s_modify(TEST_I2S_ID, LUAT_I2S_CHANNEL_RIGHT, 32, 8000);
-	luat_i2s_transfer_loop(TEST_I2S_ID, NULL, 4000, 2, 1);
+	luat_i2s_modify(TEST_I2S_ID, LUAT_I2S_CHANNEL_RIGHT, 24, 8000);
+	luat_i2s_transfer_loop(TEST_I2S_ID, (uint8_t *)test_data, 4000, 2, 1);
+	luat_event_t event;
 	while (1)
 	{
-		luat_rtos_task_sleep(10000);
+		luat_rtos_event_recv(g_s_task_handle, 0, &event, NULL, LUAT_WAIT_FOREVER);
+		switch(event.id)
+		{
+		case I2S_RX_DONE:
+			upv.u32 = event.param1;
+			DBG("%x,%x,%x,%x", upv.pu32[0], upv.pu32[1], upv.pu32[2], upv.pu32[3]);
+			break;
+		case I2S_TX_DONE:
+			break;
+
+		}
 	}
 }
 
 static void test_record_demo_init(void)
 {
-	luat_rtos_task_handle task_handle;
-	luat_rtos_task_create(&task_handle, 4096, 20, "es8311", es8311_demo_task, NULL, 0);
+	luat_rtos_task_create(&g_s_task_handle, 4096, 20, "es8311", es8311_demo_task, NULL, 16);
 }
 
 INIT_TASK_EXPORT(test_record_demo_init, "1");
